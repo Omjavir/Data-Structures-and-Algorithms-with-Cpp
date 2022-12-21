bb840e2b428c298728d51b03bce19dc34a3d8c9d
Om Suresh Javir <javirom07042003@gmail.com>
2022-12-21 22:53:57 +0530

    Learned basics of c++

---
+++ Basics.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // // *** Adding two numbers ***
+    // int amount1;
+    // int amount2;
+    // cin>>amount1;
+    // cin>>amount2;
+    // int sum = amount1 + amount2;
+    // // cout<<"The sum is: \n";
+    // cout<<"The sum is:"<<endl;
+    // // cout<<amount1 + amount2;
+    // cout<<sum;
+
+    // // *** If Else ***
+    // int number;
+    // cin>>number;
+    // if(number%2==0){
+    //     cout<<"Even Number"<<endl;
+    // }else{
+    //     cout<<"Odd Number"<<endl;
+    // }
+
+    // // *** Nested If Else ***
+    // int a, b, c;
+    // cin>>a>>b>>c;
+    // if(a>b){
+    //     if(a>c){cout<<a;}else{cout<<c;}
+    // }else{
+    //     if(b>c){cout<<b;}else{cout<<c;}
+    // }
+
+    // // *** Loops ***
+    // // *** For Loops ***
+    // int num;
+    // cin >> num;
+    // int sum = 0;
+    // for (int i = 0; i <= num; i++)
+    // {
+    //     sum += i;
+    // }
+    // cout<<sum<<endl;
+
+    // // *** While Loops ***
+    // int num;
+    // cin>>num;
+    // while(num>0){
+    //     cout<<num<<endl;
+    //     cin>>num;
+    // }
+    // cout<<"Good Bye!!\n";
+
+    // // *** Do While Loops ***
+    // // ***** Difference between While Loop and Do While loop is that do while loop will run atleast run for one time before checking for the condition. Where the While loop will first check the condiion and then run accordingly. *****
+    // int num;
+    // cin >> num;
+    // do
+    // {
+    //     cout << num << endl;
+    //     cin >> num;
+    // } while (num > 0);
+    // cout << "Good Bye!!\n";
+
+    // // *** Program to check whether the number is prime or not ***
+    // int num;
+    // cin >> num;
+    // int i;
+    // for (i = 2; i < num; i++)
+    // {
+    //     if (num % i == 0)
+    //     {
+    //         cout << "Not Prime\n";
+    //         break;
+    //     }
+    // }
+    // if (i == num)
+    // {
+    //     cout << "Prime" << endl;
+    // }
+
+    // // *** Program to check prime number in range ***
+    // int a, b;
+    // cin >> a >> b;
+    // for (int num = a; num <= b; num++)
+    // {
+    //     int i;
+    //     for (i = 2; i < num; i++)
+    //     {
+    //         if (num % i == 0)
+    //         {
+    //             break;
+    //         }
+    //     }
+    //     if (i == num)
+    //     {
+    //         cout << num << endl;
+    //     }
+    // }
+
+    return 0;
+}
